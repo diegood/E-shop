d40087a51eac5b3e83228623bfbbad66a8a0800b
Diego <diegood@live.com.ar>
2018-12-24 14:52:53 -0300

    commit inicial con todo el entorno ya configurado de Vuex, Vuetify y vueI18n

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseConfig from '@/config/firebase'
+
+import i18n from '@/config/i18n'
+import store from '@/store'
+
+firebase.initializeApp(firebaseConfig)
+export const db = firebase.firestore()
+
+require('@/config/vuetify')
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  i18n,
+  store,
+  components: { App },
+  template: '<App/>'
+})
