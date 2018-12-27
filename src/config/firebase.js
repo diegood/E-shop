b73d511afc23a18ae2758b106987ae3857427398
Diego <diegood@live.com.ar>
2018-12-27 14:19:26 -0300

    no se por que no me funciona .gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXs',
-  authDomain: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-  databaseURL: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-  projectId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-  storageBucket: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-  messagingSenderId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+  apiKey: 'AIzaSyBf91TrT4csIkxc6b6YychQXMnaPfx6j58',
+  authDomain: 'vuestoreprueba.firebaseapp.com',
+  databaseURL: 'https://vuestoreprueba.firebaseio.com',
+  projectId: 'vuestoreprueba',
+  storageBucket: 'vuestoreprueba.appspot.com',
+  messagingSenderId: '885113855523'
 }
