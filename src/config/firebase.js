a2f5e31dd308053660b4d7e158cdee750fa25d16
Diego <diegood@live.com.ar>
2018-12-27 14:40:51 -0300

    arreglo untracked files

--- src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBf91TrT4csIkxc6b6YychQXMnaPfx6j58',
-  authDomain: 'vuestoreprueba.firebaseapp.com',
-  databaseURL: 'https://vuestoreprueba.firebaseio.com',
-  projectId: 'vuestoreprueba',
-  storageBucket: 'vuestoreprueba.appspot.com',
-  messagingSenderId: '885113855523'
-}
